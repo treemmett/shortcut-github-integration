309336abda5f40f961f7714736e193b328df259f
Tregan <tregan@tregan.me>
2022-10-07 01:06:22 -0400

    feat: add github integration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config';
 import type {HttpFunction} from '@google-cloud/functions-framework/build/src/functions';
 import {verifySignature} from './security';
 import {logger} from './logger';
+import {octokit} from './github';
 
-export const shortcutGithubIntegration: HttpFunction = (req, res) => {
+export const shortcutGithubIntegration: HttpFunction = async (req, res) => {
   logger.info(req, 'New request');
   verifySignature(req);
-  res.send('Hello, World');
+  const {
+    data: {login},
+  } = await octokit.rest.users.getAuthenticated();
+  res.send(`Hello, ${login}`);
 };
