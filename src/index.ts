a0a3aeb390c32b938a0ea9e1f5c8919739fce4d0
Tregan <tregan@tregan.me>
2022-10-07 00:10:39 -0400

    fix: correct entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-// eslint-disable-next-line node/no-unpublished-import
+/* eslint-disable node/no-unpublished-import, node/no-unpublished-require */
 import type {HttpFunction} from '@google-cloud/functions-framework/build/src/functions';
 
-export const main: HttpFunction = (req, res) => {
+export const shortcutGithubIntegration: HttpFunction = (req, res) => {
   res.send('Hello, World');
 };
