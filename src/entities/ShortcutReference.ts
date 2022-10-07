b07709caf631fa70592e70f04198d3b79b41bf92
Tregan <tregan@tregan.me>
2022-10-07 15:49:18 -0400

    feat: automate issue creation

--- src/entities/ShortcutReference.ts
+++ src/entities/ShortcutReference.ts
@@ -3,9 +3,9 @@ export type EntityType = 'project' | 'workflow-state' | 'workflow-group';
 export class ShortcutReference {
   public id = 0;
 
-  public entity_name = '';
-
   public name = '';
 
   public type?: string;
+
+  public entity_type?: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import type { HttpFunction } from '@google-cloud/functions-framework/build/src/functions';
+import { ShortcutWebhook } from './entities/ShortcutWebhook';
 import { octokit } from './github';
 import { logger } from './logger';
 import { verifySignature } from './security';
@@ -7,8 +8,31 @@ import { verifySignature } from './security';
 export const shortcutGithubIntegration: HttpFunction = async (req, res) => {
   logger.info(req, 'New request');
   verifySignature(req);
-  const {
-    data: { login },
-  } = await octokit.rest.users.getAuthenticated();
-  res.send(`Hello, ${login}`);
+
+  const payload: ShortcutWebhook = req.body;
+
+  await Promise.all(
+    payload.actions.map(async (action) => {
+      if (action.action === 'create') {
+        const projectId = action.project_id;
+        if (!projectId) return;
+
+        const project = payload.reference.find(
+          (r) => r.entity_type === 'project' && r.id === projectId
+        );
+
+        if (!project) return;
+        if (project.name !== 'eXp Portal (SIDX App)') return;
+
+        await octokit.rest.issues.create({
+          body: action.description,
+          owner: 'showcase-idx',
+          repo: 'consumer-ui',
+          title: action.name,
+        });
+      }
+    })
+  );
+
+  res.send('ok');
 };
