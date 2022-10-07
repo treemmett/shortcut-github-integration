769783d4b3545a8659f7fd34ef05e3deb4835316
Tregan <tregan@tregan.me>
2022-10-07 00:33:43 -0400

    feat(logger): add logger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import type {HttpFunction} from '@google-cloud/functions-framework/build/src/functions';
 import {verifySignature} from './security';
+import {logger} from './logger';
 
 export const shortcutGithubIntegration: HttpFunction = (req, res) => {
+  logger.info('New request', {req});
   verifySignature(req);
   res.send('Hello, World');
 };
