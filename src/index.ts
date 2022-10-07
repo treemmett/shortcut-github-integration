d316b1c6a7ba55facf8fcfde9facd8780ffd0782
Tregan <tregan@tregan.me>
2022-10-07 00:29:51 -0400

    chore(lint): disable published rule

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-/* eslint-disable node/no-unpublished-import, node/no-unpublished-require */
 import type {HttpFunction} from '@google-cloud/functions-framework/build/src/functions';
+import {verifySignature} from './security';
 
 export const shortcutGithubIntegration: HttpFunction = (req, res) => {
+  verifySignature(req);
   res.send('Hello, World');
 };
