d2d53996388b0e1a060336cb5e7726b36adb8684
Tregan <tregan@tregan.me>
2022-10-09 00:02:44 -0400

    refactor: add shortcut handler

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,10 @@
 import 'dotenv/config';
 import type { HttpFunction } from '@google-cloud/functions-framework/build/src/functions';
-import { ShortcutWebhook } from './entities/ShortcutWebhook';
-import { octokit } from './github';
 import { logger } from './logger';
+import { handleWebhook } from './shortcut';
 
 export const shortcutGithubIntegration: HttpFunction = async (req, res) => {
   logger.info({ body: req.body, req }, 'New request');
-
-  const payload: ShortcutWebhook = req.body;
-
-  await Promise.all(
-    payload.actions.map(async (action) => {
-      if (action.action === 'create') {
-        const projectId = action.project_id;
-        if (!projectId) return;
-
-        const project = payload.references?.find(
-          (r) => r.entity_type === 'project' && r.id === projectId
-        );
-
-        if (!project) return;
-        if (project.name !== 'eXp Portal (SIDX App)') return;
-        if (
-          !payload.references?.find(
-            (r) => r.entity_type === 'group' && r.name === 'Engineering-FrontEnd'
-          )
-        )
-          return;
-
-        await octokit.rest.issues.create({
-          body: `${action.description}\n\n${action.app_url}`,
-          owner: 'showcaseidx',
-          repo: 'consumer-ui',
-          title: action.name,
-        });
-      }
-    })
-  );
-
+  await handleWebhook(req.body);
   res.send('ok');
 };
---
+++ src/shortcut.ts
@@ -0,0 +1,33 @@
+import { ShortcutWebhook } from './entities/ShortcutWebhook';
+import { octokit } from './github';
+
+export async function handleWebhook(webhook: ShortcutWebhook): Promise<void> {
+  await Promise.all(
+    webhook.actions.map(async (action) => {
+      if (action.action === 'create') {
+        const projectId = action.project_id;
+        if (!projectId) return;
+
+        const project = webhook.references?.find(
+          (r) => r.entity_type === 'project' && r.id === projectId
+        );
+
+        if (!project) return;
+        if (project.name !== 'eXp Portal (SIDX App)') return;
+        if (
+          !webhook.references?.find(
+            (r) => r.entity_type === 'group' && r.name === 'Engineering-FrontEnd'
+          )
+        )
+          return;
+
+        await octokit.rest.issues.create({
+          body: `${action.description}\n\n${action.app_url}`,
+          owner: 'showcaseidx',
+          repo: 'consumer-ui',
+          title: action.name,
+        });
+      }
+    })
+  );
+}
